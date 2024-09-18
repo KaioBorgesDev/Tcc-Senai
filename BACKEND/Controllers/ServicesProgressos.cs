f4df79b8a2c2dfe23db810b56419c6113321518f
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-09-18 16:58:37 -0300

    Fix- Projeto com o Backend Corrigido

---
+++ BACKEND/Controllers/ServicesProgressos.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using senai_game.Models;
+
+namespace senai_game.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServicesProgressos : ControllerBase
+    {
+
+        [HttpGet("/Progresso/{id}")]
+        public IActionResult GetById(int id)
+        {
+            
+            var progresso = Progresso.getById(id);
+
+            if (progresso != null) 
+                return Ok(progresso);
+            return Ok("Progresso não encontrado");
+        }
+
+        [HttpGet("/Progresso")]
+        public IActionResult GetAllProgresso()
+        {
+            var progresso = Progresso.getAllProgresso();
+
+            if (progresso != null)
+                return Ok(progresso);
+            return Ok("Progresso não encontrado");
+        }
+
+        
+
+
+    }
+
+ 
+}
---
+++ BACKEND/README.md
@@ -0,0 +1,4 @@
+
+=======
+# Senai-Game-Api
+>>>>>>> backend/main
