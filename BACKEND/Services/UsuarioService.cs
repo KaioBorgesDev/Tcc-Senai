39f864e1e9b9562f1ee94aa8d8f08e4aa12b9e5c
KAIO OLIVEIRA <kaio.oliveira14@senaisp.edu.br>
2024-10-18 16:56:23 -0300

    feat- Services e Controller de Usuarios, Incompleto, esperando um --amend

---
+++ BACKEND/Services/UsuarioService.cs
@@ -0,0 +1,28 @@
+﻿using senai_game.DTOs;
+using senai_game.Models;
+using senai_game.Repositories;
+
+namespace senai_game.Services
+{
+    public class UsuarioService
+    {
+
+        private readonly UserRepository _repository;
+
+        public UsuarioService(UserRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public string CreateUser(UserDTO userDTO)
+        {
+            User user = new User(userDTO.Email, userDTO.Password, userDTO.Username);
+            return _repository.InsertUser(user);       
+        }
+
+        public User Login(string email, string password)
+        {
+            return _repository.GetUser(email, password);
+        }
+    }
+}
