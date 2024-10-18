340555d36c6ad55faaaa4eb51ed92e267f3f1a67
KAIO OLIVEIRA <kaio.oliveira14@senaisp.edu.br>
2024-10-18 16:55:09 -0300

    fix- Data Transfer Object dos Usuarios

---
+++ BACKEND/DTOs/UserDTO.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai_game.DTOs
+{
+    public class UserDTO
+    {
+        [Required]
+        [EmailAddress]
+        private string email;
+
+        [Required]
+        [MinLength(6)]
+        private string password;
+
+        [Required]
+        private string username;
+
+
+        private string status;
+        private string rules;
+
+
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Username { get => username; set => username = value; }
+        public string Status { get => status; set => status = value; }
+        public string Rules { get => rules; set => rules = value; }
+
+
+    }
+}
---
+++ BACKEND/DTOs/UserLoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai_game.DTOs
+{
+    public class UserLoginDTO
+    {
+
+        [Required]
+        [EmailAddress]
+        public string email { get;}
+
+        [Required]
+        [MinLength(6)]
+        public string password { get; }
+
+
+    }
+}
