cf5b7cd67878d4ac67c2cca2097221ea75006aed
KAIO OLIVEIRA <kaio.oliveira8@fatec.sp.gov.br>
2024-10-19 09:54:29 -0300

    feat- favoriteDTO com validações

---
+++ BACKEND/DTOs/FavoriteDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace senai_game.DTOs
+{
+    public class FavoriteDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string email_user { get; set; }
+        [Required]
+        [RegularExpression(@"^\d+$", ErrorMessage = "O valor do id da prova, deve ser um numero!")]
+        public int prova_fav { get; set; }
+        [Required]
+        [MaxLength(45)]
+        public string titulo_prova { get; set; }
+    }
+}
