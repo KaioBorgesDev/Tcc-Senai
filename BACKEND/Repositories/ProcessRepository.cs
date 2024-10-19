7f00f6d29f0ea9a772b789a28c8b8ecb066b9792
KAIO OLIVEIRA <kaio.oliveira8@fatec.sp.gov.br>
2024-10-19 11:05:40 -0300

    fix- ProcessRepository e ServiceRepository

---
+++ BACKEND/Repositories/ProcessRepository.cs
@@ -0,0 +1,130 @@
+﻿using MySql.Data.MySqlClient;
+using senai_game.Factory;
+using senai_game.Models;
+
+namespace senai_game.Repositories
+{
+    public class ProcessRepository
+    {
+        private readonly MySqlConnection _connection;
+
+        public ProcessRepository()
+        {
+            string connection_env = Environment.GetEnvironmentVariable("CONEXAO", EnvironmentVariableTarget.User);
+
+            if (connection_env == null)
+                connection_env = "senai";
+
+            _connection = FactoryConnection.getConnection(connection_env);
+        }
+
+        internal List<Process> GetAllProcess()
+        {
+            var allProcessos = new List<Process>();
+            try
+            {
+                _connection.Open();
+                MySqlCommand command = new MySqlCommand("Select * from processos", _connection);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        Process processo = new Process((int)reader["id"],
+                            reader["name"].ToString(),
+                            reader["description"].ToString(),
+                            reader["semestre"].ToString(),
+                            perguntas: Question.getById((int)reader["id"])
+                        );
+                        allProcessos.Add(processo);
+                    }
+                    return allProcessos;
+                }
+                return allProcessos;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Falha ao procurar todos os processos" + ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        internal int GetIdByProcess(Process process)
+        {
+            try
+            {
+                _connection.Open();
+                MySqlCommand command = new MySqlCommand("Select id from processos where name = @name and description = @description and semestre = @semestre", _connection);
+                command.Parameters.AddWithValue("@name", process.Name);
+                command.Parameters.AddWithValue("@description", process.Description);
+                command.Parameters.AddWithValue("@semestre", process.Semestre);
+
+                MySqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                if (reader["id"].ToString() != null)
+                {
+                    return (int)reader["id"];
+                }
+                return -1;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao pegar o id do processo" + ex.Message);
+            }
+            finally
+            {
+                _connection.Close();
+            }
+        }
+
+        internal Process GetProcessById(int id)
+        {
+            try
+            {
+                _connection.Open();
+                MySqlCommand command = new MySqlCommand("Select * from processos where id = @id", _connection);
+                command.Parameters.AddWithValue("@id", id);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    return new Process((int)reader["id"], reader["name"].ToString(), reader["description"].ToString(), reader["semestre"].ToString(), perguntas: Question.getById((int)reader["id"]));
+                }
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao procurar processo com o id " + id + " \n" + ex.Message);
+            }
+        }
+
+        internal string InsertProcess(Process process)
+        {
+            try
+            {
+                _connection.Open();
+
+                MySqlCommand command = new MySqlCommand("insert into processos (name, description, semestre) values (@name, @description, @semestre)", _connection);
+                command.Parameters.AddWithValue("@name", process.Name);
+                command.Parameters.AddWithValue("@description", process.Description);
+                command.Parameters.AddWithValue("@semestre", process.Semestre);
+
+                command.ExecuteNonQuery();
+                return "Processo inserido com sucesso";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao inserir o processo..." + ex.Message);
+            }finally { 
+                _connection.Close();
+            }
+        }
+    }
+}
---
+++ BACKEND/Services/ServiceProcess.cs
@@ -0,0 +1,40 @@
+﻿using senai_game.DTOs;
+using senai_game.Models;
+using senai_game.Repositories;
+
+namespace senai_game.Services
+{
+    public class ServiceProcess
+    {
+        private readonly ProcessRepository _repository;
+
+        public ServiceProcess()
+        {
+            _repository = new ProcessRepository();
+        }
+
+        public string CreateProcess(ProcessDTO processDTO)
+        {
+           
+            var process = new Process(processDTO.id, processDTO.name, processDTO.description, processDTO.semestre);
+
+            return _repository.InsertProcess(process);
+        }
+
+        internal List<Process> GetAllProcess()
+        {
+            return _repository.GetAllProcess();
+        }
+
+        internal int GetIdByProcess(ProcessDTO processDTO)
+        {
+            var process = new Process(processDTO.name, processDTO.description, processDTO.semestre);
+            return _repository.GetIdByProcess(process);
+        }
+
+        internal Process GetProcessById(int id)
+        {
+            return _repository.GetProcessById(id);
+        }
+    }
+}
