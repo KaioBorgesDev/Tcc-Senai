f639f33783b65b2974ac104f5d3d562d8bcc12b0
KAIO OLIVEIRA <kaio.oliveira8@fatec.sp.gov.br>
2024-10-19 09:53:59 -0300

    fix- model refatorado

---
+++ BACKEND/Models/Favorite.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text.Json.Serialization;
+
+namespace senai_game.Models
+{
+    public class Favorite
+    {
+        private string email_user;
+        private int prova_fav;
+        private string titulo_prova;
+
+
+        
+        public Favorite(string email_user, int prova_fav, string titulo_prova)
+        {
+            this.email_user = email_user;
+            this.prova_fav = prova_fav;
+            this.titulo_prova = titulo_prova;
+        }
+
+        public Favorite(int prova_fav, string titulo_prova)
+        {
+            this.titulo_prova = titulo_prova;
+            this.prova_fav = prova_fav;
+        }
+
+        public string Email_user { get => email_user;  }
+        public int Prova_fav { get => prova_fav; }
+        public string Titulo_prova { get => titulo_prova;}
+
+    }
+}
