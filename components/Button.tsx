2589c77f6a4f1d97d541e89673ac84116a5d83fb
KAIO OLIVEIRA <kaio.oliveira8@fatec.sp.gov.br>
2024-12-08 17:58:49 -0300

    fix- button corrigido

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,42 +1,46 @@
 import { useState } from 'react';
-import {StyleSheet, Text} from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
 
-interface ButtonProps{
-    resposta: string,
-    correta: boolean,
-    firstTry: boolean,
-    onPress: () => void
+interface ButtonProps {
+  resposta: string;
+  correta: boolean;
+  firstTry: boolean;
+  onPress: () => void;
 }
-const Button : React.FC<ButtonProps> = ({resposta, correta, onPress, firstTry}) => {
-    const [buttonColor, setButtonColor] = useState('white');
+
+const Button: React.FC<ButtonProps> = ({ resposta, correta, onPress, firstTry }) => {
+  const [buttonColor, setButtonColor] = useState('white');
+
   // Função para alterar a cor do botão
-    const handlePress = () => {
-      //Esta com um bug, se a pessoa aperta duas vezes, volta pro branco, arrumar isto
-    setButtonColor(firstTry ? correta ? 'green' : 'red' : 'gray');
+  const handlePress = () => {
+    // Arrumar o bug de color reset
+    setButtonColor(firstTry ? (correta ? 'green' : 'red') : 'gray');
     onPress();
   };
 
   return (
     <TouchableOpacity onPress={handlePress} style={[styles.button, { backgroundColor: buttonColor }]}>
-        
-        <Text style={styles.buttonText}>{resposta}</Text>
+      <Text style={styles.buttonText}>{resposta}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
+
+const { width } = Dimensions.get('window'); // Pegando a largura da tela
+
 const styles = StyleSheet.create({
   button: {
     margin: 10,
-    padding: 15,
+    paddingVertical: 15,
+    paddingHorizontal: width * 0.1, // A largura do botão é 80% da tela
     alignSelf: 'center',
     borderRadius: 8,
     borderWidth: 1,
-
+    minWidth: 200, // Garantindo que o botão tenha uma largura mínima
   },
   buttonText: {
     color: 'black',
     textAlign: 'center',
-    fontSize: 14,
+    fontSize: width * 0.04, // Ajustando o tamanho da fonte com base na largura da tela
     fontFamily: 'Sans-Serif',
   },
   ListOpcao: {
@@ -62,5 +66,4 @@ const styles = StyleSheet.create({
   },
 });
 
-
-export default Button
+export default Button;
