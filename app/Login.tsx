603016954d75d41448d7557c8ca80dbfc06aaf8c
KAIO OLIVEIRA <kaio.oliveira8@fatec.sp.gov.br>
2024-12-08 10:10:13 -0300

    fix- login com responsividade

--- app/Login.tsx
+++ app/Login.tsx
@@ -15,30 +15,38 @@ const SignIn = () => {
 
   return (
     <View>
-          <View style={styles.logo}>
-              <Image source={require('../assets/images/logo 1.svg')} style={styles.image}></Image>
-              <Image source={require('../assets/images/Game 1.svg')} style={styles.image}></Image>
-
-          </View>
-          <View style={styles.FormSingUp}>
-          <Text style={styles.TituloInput}> EMAIL:</Text>
-              <TextInput placeholder="example@.com" style={styles.inputText} onChangeText={setEmail}/>
-              <Text style={styles.TituloInput}> PASSWORD:</Text>
-                      <TextInput secureTextEntry={true} placeholder="*****" style={styles.inputText} onChangeText={setPassword}/>
-                          <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 10}}>
-                          <Link style={{top:15, textDecorationLine: "underline",color:'#65558F'}} href={'/SignUp'}><Text style={styles.TituloInput}>Crie uma conta</Text></Link>
-                          <Link style={{top:15, textDecorationLine: "underline",color:'#65558F'}} href={'/Login'}><Text style={styles.TituloInput}>Esqueceu sua senha?</Text></Link>
-                          
-                          </View>
-              </View>
-              
-               <TouchableOpacity style={styles.ButtonCad} onPress={() => HandleSignIn()} >
-                  <Text style={{color: 'white', textAlign: 'center',}}>Enviar</Text>
-              </TouchableOpacity>
-
-      </View>
-
-  )
+        <View style={styles.logo}>
+            <Image source={require('../assets/images/logo 1.png')} style={styles.image}></Image>
+            <Image source={require('../assets/images/Game 1.png')} style={styles.image}></Image>
+        </View>
+        <View style={styles.formSignUp}>
+            <Text style={styles.titleInput}>EMAIL:</Text>
+            <TextInput
+                placeholder="example@.com"
+                style={styles.inputText}
+                onChangeText={setEmail}
+            />
+            <Text style={styles.titleInput}>PASSWORD:</Text>
+            <TextInput
+                secureTextEntry={true}
+                placeholder="*****"
+                style={styles.inputText}
+                onChangeText={setPassword}
+            />
+            <View style={styles.linksContainer}>
+                <Link href={'/SignUp'} style={styles.link}>
+                    Crie uma conta
+                </Link>
+                <Link href={'/Login'} style={styles.link}>
+                    Esqueceu sua senha?
+                </Link>
+            </View>
+        </View>  
+        <TouchableOpacity style={styles.ButtonCad} onPress={() => HandleSignIn()} >
+            <Text style={{color: 'white', textAlign: 'center',}}>Enviar</Text>
+        </TouchableOpacity>
+    </View>
+   )
 }
 
 const styles = StyleSheet.create({
@@ -48,15 +56,17 @@ const styles = StyleSheet.create({
   ,logo: {
       justifyContent: 'center',
       alignItems: 'center',
-      marginTop: 180,
+      marginTop: 100,
   }, 
   inputText: {
-      width: 350,
-      height: 40,
-      backgroundColor: 'white',
-      borderRadius: 10,
-      left: 60,
-      paddingLeft: 20,
+    width: '100%', // Usa a largura total do contêiner
+    height: 50,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    paddingLeft: 20,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
   },
   TituloInput: {
       marginTop: 20,
@@ -74,6 +84,32 @@ const styles = StyleSheet.create({
       borderRadius: 10,
       padding: 10,
       backgroundColor: '#4a4a4a',
+      marginBottom: 500,
+  },
+  formSignUp: {
+    maxWidth: 500,
+    width: '90%', // Adapta ao tamanho da tela
+    alignSelf: 'center',
+    marginTop: 20,
+  },
+  titleInput: {
+    fontSize: 16,
+    color: '#333',
+    marginBottom: 8,
+    marginLeft: '5%',
+  },
+  
+  linksContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    paddingHorizontal: '5%',
+  },
+  link: {
+    fontSize: 14,
+    color: '#65558F',
+    textDecorationLine: 'underline',
+    marginTop: 10,
   },
 })
 export default SignIn;
